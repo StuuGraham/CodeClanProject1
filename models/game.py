ca4bcc070caacf231bb2654675d070aa26ae40ec
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-11-08 20:51:37 +0000

    made Game class file and added psuedocode to denote plan for class

---
+++ models/game.py
@@ -0,0 +1,5 @@
+class Game:
+
+    # This will function similar to the Rock, Paper, Scissors homework.
+    # Function should take in two teams and produce a winner at random.
+    # Will need Randint?
