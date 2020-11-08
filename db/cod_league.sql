bf08e831d91926f618e59b0a6e95140afc2f9cba
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-11-08 21:44:36 +0000

    Initial info for sql file is set-up

--- db/cod_league.sql
+++ db/cod_league.sql
@@ -3,13 +3,18 @@ DROP TABLE games;
 DROP TABLE teams;
 
 CREATE TABLE teams (
-
+    id SERIAL PRIMARY KEY,
+    city VARCHAR(255),
+    name VARCHAR(255),
+    points INT
 );
 
 CREATE TABLE games (
-
+    id SERIAL PRIMARY KEY,
 );
 
 CREATE TABLE league (
-
+    id SERIAL PRIMARY KEY,
+    team_id INT REFERENCES teams(id) ON DELETE CASCADE,
+    game_id INT REFERENCES games(id) ON DELETE CASCADE
 );
