4ec6ca9e37ee722b04c3be295071c398175af84f
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-11-09 14:41:35 +0000

    app.py now corrected and active

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-# from flask import Flask, render_template
+from flask import Flask, render_template
 
-# from controllers.league_controller import league_blueprint
-# from controllers.game_controller import games_blueprint
-# from controllers.team_controller import teams_blueprint
+from controllers.leagues_controller import leagues_blueprint
+from controllers.matches_controller import matches_blueprint
+from controllers.teams_controller import teams_blueprint
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# app.register_blueprint(league_blueprint)
-# app.register_blueprint(games_blueprint)
-# app.register_blueprint(teams_blueprint)
+app.register_blueprint(league_blueprint)
+app.register_blueprint(matches_blueprint)
+app.register_blueprint(teams_blueprint)
 
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
