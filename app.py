a26034ff19d374a32ed7c8e8e15bce35875ce3f2
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-11-09 09:57:27 +0000

    corrected app.py references, not active yet. Made controller files

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # from flask import Flask, render_template
 
-# from controllers.visit_controller import visits_blueprint
-# from controllers.location_controller import locations_blueprint
-# from controllers.user_controller import users_blueprint
+# from controllers.league_controller import league_blueprint
+# from controllers.game_controller import games_blueprint
+# from controllers.team_controller import teams_blueprint
 
 # app = Flask(__name__)
 
-# app.register_blueprint(visits_blueprint)
-# app.register_blueprint(locations_blueprint)
-# app.register_blueprint(users_blueprint)
+# app.register_blueprint(league_blueprint)
+# app.register_blueprint(games_blueprint)
+# app.register_blueprint(teams_blueprint)
 
 # @app.route('/')
 # def home():
